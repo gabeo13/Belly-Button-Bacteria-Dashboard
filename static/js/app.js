3e9d8a91a13062b3de497702dfe7523a5f73ee45
Gabriel Allen <gabriel.houston.allen@gmail.com>
2021-03-29 15:30:23 -0600

    update dashboard

--- static/js/app.js
+++ static/js/app.js
@@ -1,155 +1,145 @@
-//Grab List of Sample Names
-idArray = d3.json(`../../data/samples.json`, function (data) { data['names'] });
-console.log(idArray);
+//Read in JSON file and establish dropdown menu of options
+var jsonPath = `../../data/samples.json`;
+d3.json(jsonPath).then(function (data) {
+    console.log(data);
 
-// Fill dropdown selection menu
-function assignOptions(textArray, selector) {
-    for (var i = 0; i < textArray.length; i++) {
-        var currentOption = document.createElement('option');
-        currentOption.text = textArray[i];
-        selector.appendChild(currentOption);
-    }
-}
+    var names = Object.values(data.names);
+    var dropDownlist = document.getElementById('selDataset');
+    for (i = 0; i < names.length; i++) {
+        dropDownlist.options[i] = new Option(names[i]);
+    };
+});
 
-assignOptions(listofCountries, countrySelector);
+// Initialize Dashboard with an ID
+function init() {
+    buildPlot('940');
+};
 
-countrySelector.addEventListener('change', updateCountry, false);
+init();
 
-// Assign User Selection to Variable
-var id = d3.select('#selDataset');
+// Connect dropdown menu to plot constructor
+function optionChanged() {
+    dropDownMenu = d3.select('#selDataset');
+    id = dropDownMenu.property('value');
+    console.log(id);
+    buildPlot(id);
+};
 
-// Fetch data from samples.json
+optionChanged();
 
+
+// Unpack function for listed arrays
 function unpack(rows, index) {
     return rows.map(function (row) {
         return row[index];
     });
-}
+};
 
-function getJsonData(id) {
+
+
+function buildPlot(id) {
 
     var jsonPath = `../../data/samples.json`;
     d3.json(jsonPath).then(function (data) {
         console.log(data);
-        if (data['samples']['id'] == id && data['metadata']['id'] == id) {
-            var sampleValues = data['samples']['sample_values'];
-            var otuIds = data['samples']['otu_ids'];
-            var otuLabels = data['samples']['otu_labels'];
-            var id = unpack(data['metadata'], 0);
-            var ethnicity = unpack(data['metadata'], 1);
-            var age = unpack(data['metadata'], 2);
-            var location = unpack(data['metadata'], 3);
-            var bbtype = unpack(data['metadata'], 4);
-            var wfreq = unpack(data['metadata'], 5);
-            buildPanel(id, ethnicity, age, location, bbtype, wfreq)
+        var sampleSet = data['samples'];
+        console.log(sampleSet);
+
+        var chosenData = sampleSet.filter(record => record.id == id);
+        console.log(chosenData);
+
+        var sampleValues = chosenData[0]['sample_values'];
+        var otuLabels = chosenData[0]['otu_labels'];
+        var otuIds = chosenData[0]['otu_ids'];
+
+        // Horizontal Bar Chart
+        var trace1 = {
+            y: otuIds,
+            x: sampleValues,
+            type: 'bar',
+            hovertext: otuLabels,
+            orientation: 'h'
         };
-    });
-};
 
-function buildPanel(id, ethnicity, age, location, bbtype, wfreq) {
-    var table = d3.select("#sample-metadata");
-    var tbody = table.select("tbody");
-    var trow;
-    for (var i = 0; i < 1; i++) {
-        trow = tbody.append("tr");
-        trow.append("td").text(id[i]);
-        trow.append("td").text(ethnicity[i]);
-        trow.append("td").text(age[i]);
-        trow.append("td").text(location[i]);
-        trow.append("td").text(bbtype[i]);
-        trow.append("td").text(wfreq[i]);
-    }
-};
+        var data = [trace1];
 
-// Horizontal Bar Chart
-var trace1 = {
-    y: ['Liam', 'Sophie', 'Jacob', 'Mia', 'William', 'Olivia'],
-    x: [8.0, 8.0, 12.0, 12.0, 13.0, 20.0],
-    type: 'bar',
-    text: ['4.17 below the mean', '4.17 below the mean', '0.17 below the mean', '0.17 below the mean', '0.83 above the mean', '7.83 above the mean'],
-    marker: {
-        color: 'rgb(142,124,195)'
-    },
-    orientation: 'h'
-};
-
-var data = [trace1];
-
-var layout = {
-    title: 'Number of Graphs Made this Week',
-    font: {
-        family: 'Raleway, sans-serif'
-    },
-    showlegend: false,
-    xaxis: {
-        tickangle: -45
-    },
-    yaxis: {
-        zeroline: false,
-        gridwidth: 2
-    },
-    bargap: 0.05
-};
-
-Plotly.newPlot('bar', data, layout);
-
-// Bubble Chart 
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-    mode: 'markers',
-    marker: {
-        color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-        size: [40, 60, 80, 100]
-    }
-};
-
-var data = [trace1];
-
-var layout = {
-    title: 'Bubble Chart Hover Text',
-    showlegend: false,
-    height: 600,
-    width: 600
-};
-
-Plotly.newPlot('bubble', data, layout);
-
-
-// Gauge Chart
-var data = [
-    {
-        type: "indicator",
-        mode: "gauge+number+delta",
-        value: 420,
-        title: { text: "Speed", font: { size: 24 } },
-        delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-        gauge: {
-            axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-            bar: { color: "darkblue" },
-            bgcolor: "white",
-            borderwidth: 2,
-            bordercolor: "gray",
-            steps: [
-                { range: [0, 250], color: "cyan" },
-                { range: [250, 400], color: "royalblue" }
-            ],
-            threshold: {
-                line: { color: "red", width: 4 },
-                thickness: 0.75,
-                value: 490
+        var layout = {
+            title: 'Sample Value by ID',
+            font: {
+                family: 'Raleway, sans-serif'
+            },
+            // showlegend: false,
+            xaxis: {
+                tickangle: -45
             }
-        }
-    }
-];
+        };
 
-var layout = {
-    width: 500,
-    height: 400,
-    margin: { t: 25, r: 25, l: 25, b: 25 },
-    paper_bgcolor: "lavender",
-    font: { color: "darkblue", family: "Arial" }
+        Plotly.newPlot('bar', data, layout);
+
+    });
+
+    // // Bubble Chart 
+    // var trace1 = {
+    //     x: [1, 2, 3, 4],
+    //     y: [10, 11, 12, 13],
+    //     text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+    //     mode: 'markers',
+    //     marker: {
+    //         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+    //         size: [40, 60, 80, 100]
+    //     }
+    // };
+
+    // var data = [trace1];
+
+    // var layout = {
+    //     title: 'Bubble Chart Hover Text',
+    //     showlegend: false,
+    //     height: 600,
+    //     width: 600
+    // };
+
+    // Plotly.newPlot('bubble', data, layout);
+
+
+    // // Gauge Chart
+    // var data = [
+    //     {
+    //         type: "indicator",
+    //         mode: "gauge+number+delta",
+    //         value: 420,
+    //         title: { text: "Speed", font: { size: 24 } },
+    //         delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+    //         gauge: {
+    //             axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
+    //             bar: { color: "darkblue" },
+    //             bgcolor: "white",
+    //             borderwidth: 2,
+    //             bordercolor: "gray",
+    //             steps: [
+    //                 { range: [0, 250], color: "cyan" },
+    //                 { range: [250, 400], color: "royalblue" }
+    //             ],
+    //             threshold: {
+    //                 line: { color: "red", width: 4 },
+    //                 thickness: 0.75,
+    //                 value: 490
+    //             }
+    //         }
+    //     }
+    // ];
+
+    // var layout = {
+    //     width: 500,
+    //     height: 400,
+    //     margin: { t: 25, r: 25, l: 25, b: 25 },
+    //     paper_bgcolor: "lavender",
+    //     font: { color: "darkblue", family: "Arial" }
+    // };
+
+    // Plotly.newPlot('gauge', data, layout);
+
 };
 
-Plotly.newPlot('gauge', data, layout);
+
+
