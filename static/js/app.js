97d601e2c3687ae54e64437f529583ee84347133
Gabriel Allen <gabriel.houston.allen@gmail.com>
2021-03-30 11:09:46 -0600

    update with gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,7 @@ function init() {
 
 init();
 
-// Connect dropdown menu to plot constructor
+// Connect html dropdown menu to plot constructor function
 function optionChanged() {
     dropDownMenu = d3.select('#selDataset');
     id = dropDownMenu.property('value');
@@ -28,12 +28,12 @@ function optionChanged() {
 optionChanged();
 
 
-// Unpack function for listed arrays
-function unpack(rows, index) {
-    return rows.map(function (row) {
-        return row[index];
-    });
-};
+// // Unpack function for listed arrays
+// function unpack(rows, index) {
+//     return rows.map(function (row) {
+//         return row[index];
+//     });
+// };
 
 
 // Extract Transform and Load Desired JSON Data Into Dashboard Elements
@@ -57,16 +57,16 @@ function buildPlot(id) {
         var wayTooMeta = Object.entries(chosenMeta[0]);
         console.log(wayTooMeta);
 
-        // reformat the info  
+        // Format Metadata  
         const cleanedMeta = wayTooMeta.map(entry => entry.join(': '));
 
-        // clear existing list
+        // Reset List
         d3.select('#sample-metadata>ul').remove();
 
-        // create a list of subject's demographic data
+        // Build UL Tag in index.html using d3 command
         const metaList = d3.select('#sample-metadata').append('ul');
 
-        // append demographic info to metadata panel
+        // Append li tags and content to ul
         cleanedMeta.forEach(item => {
             var itemText = metaList.append('li');
             itemText.text(`${item}`);
@@ -102,7 +102,7 @@ function buildPlot(id) {
         var data = [trace1];
 
         var layout = {
-            title: 'Sample Value by ID',
+            title: 'Top 10 OTU IDs by Sample Value',
             font: {
                 family: 'Raleway, sans-serif'
             },
@@ -132,51 +132,59 @@ function buildPlot(id) {
         var layout = {
             title: 'Belly Button Bubble Chart',
             showlegend: false,
+            xaxis: {
+                title: 'OTU IDs'
+            },
+            yaxis: {
+                title: 'Sample Value'
+            }
         };
 
         Plotly.newPlot('bubble', data, layout);
 
+        // Gauge Chart
+        var data = [
+            {
+                type: "indicator",
+                mode: "gauge+number",
+                value: chosenMeta[0]['wfreq'],
+                title: { text: "Belly Button Wash Frequencey (d/wk)", font: { size: 24 } },
+                gauge: {
+                    axis: {
+                        range: [null, 9], tickmode: 'linear', tickwidth: 1, tickcolor: "#34495e", tickfont: {
+                            size: 18, family: 'Raleway, sans-serif'
+                        }
+                    },
+                    bar: { color: "#34495e" },
+                    bgcolor: "white",
+                    borderwidth: 2,
+                    bordercolor: "#34495e",
+                    steps: [
+                        { range: [0, 1], color: '#e74c3c' },
+                        { range: [1, 2], color: '#d35400' },
+                        { range: [2, 3], color: '#e67e22' },
+                        { range: [3, 4], color: '#f39c12' },
+                        { range: [4, 5], color: '#f1c40f' },
+                        { range: [5, 6], color: '#27ae60' },
+                        { range: [6, 7], color: '#2ecc71' },
+                        { range: [7, 8], color: '#3498db' },
+                        { range: [8, 9], color: '#9b59b6' },
+                    ],
+
+                }
+            }
+        ];
+
+        var layout = {
+            width: 500,
+            height: 400,
+            margin: { t: 25, r: 25, l: 25, b: 25 },
+            font: { color: "#34495e", family: "Raleway, sans-serif" }
+        };
+
+        Plotly.newPlot('gauge', data, layout);
+
     });
-
-
-
-
-    // // Gauge Chart
-    // var data = [
-    //     {
-    //         type: "indicator",
-    //         mode: "gauge+number+delta",
-    //         value: 420,
-    //         title: { text: "Speed", font: { size: 24 } },
-    //         delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-    //         gauge: {
-    //             axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-    //             bar: { color: "darkblue" },
-    //             bgcolor: "white",
-    //             borderwidth: 2,
-    //             bordercolor: "gray",
-    //             steps: [
-    //                 { range: [0, 250], color: "cyan" },
-    //                 { range: [250, 400], color: "royalblue" }
-    //             ],
-    //             threshold: {
-    //                 line: { color: "red", width: 4 },
-    //                 thickness: 0.75,
-    //                 value: 490
-    //             }
-    //         }
-    //     }
-    // ];
-
-    // var layout = {
-    //     width: 500,
-    //     height: 400,
-    //     margin: { t: 25, r: 25, l: 25, b: 25 },
-    //     paper_bgcolor: "lavender",
-    //     font: { color: "darkblue", family: "Arial" }
-    // };
-
-    // Plotly.newPlot('gauge', data, layout);
 
 };
 
